0c65efbc148e2e7d6007a335428dd6d15ccac4b6
Venkatesh K <venkateshk14046@gmail.com>
2025-04-28 21:55:56 +0530

    Spring - project initiated

---
+++ spring-framework-in10steps/spring-framework-in10steps/src/main/java/com/tech/spring_framework_in10steps/SpringFrameworkIn10stepsApplication.java
@@ -0,0 +1,13 @@
+package com.tech.spring_framework_in10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkIn10stepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkIn10stepsApplication.class, args);
+	}
+
+}
---
+++ spring-framework-in10steps/spring-framework-in10steps/src/test/java/com/tech/spring_framework_in10steps/SpringFrameworkIn10stepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.spring_framework_in10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkIn10stepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
